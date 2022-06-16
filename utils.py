2b3c375a253794dce7a6dd62fa774e7fc7bdd76e
Ridwan Rahman <ridwan@clipsalsolar.com>
2022-06-16 19:44:40 +1000

    Added more comments

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ from enum import Enum
 
 
 class DirectionsEnum(Enum):
+    """
+    Enum to hold the different directions
+    """
     NORTH = 0
     EAST = 1
     SOUTH = 2
@@ -10,5 +13,9 @@ class DirectionsEnum(Enum):
 
 
 def get_root_folder():
+    """
+    Get the root directory of the project.
+    To maintain the same path to the root
+    """
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     return ROOT_DIR
