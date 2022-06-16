3d62e01736a6bb1684c13b849a651e4403a0d174
Ridwan Rahman <ridwan@clipsalsolar.com>
2022-06-16 15:37:52 +1000

    Fixed end to end test folder issue

--- tests/test_full_program.py
+++ tests/test_full_program.py
@@ -1,5 +1,7 @@
+import os
 import pytest
 import logging
+import utils
 
 from toy_robot.command_handler import CommandHandler
 
@@ -8,9 +10,12 @@ class TestEndToEnd:
 
     def test_end_to_end_1(self, caplog):
         caplog.set_level(logging.INFO)
-        file_name = '../resources/test_inputs/test_user_inputs_1.txt'
+
+        root_folder = utils.get_root_folder()
+        file_path = os.path.join(root_folder, 'resources/test_inputs/test_user_inputs_1.txt')
+
         test_command_class = CommandHandler()
-        test_command_class.set_file_name(file_name)
+        test_command_class.set_file_name(file_path)
         test_command_class.command_runner()
         assert (
                 "1, 4, EAST"
@@ -19,9 +24,12 @@ class TestEndToEnd:
 
     def test_end_to_end_2(self, caplog):
         caplog.set_level(logging.INFO)
-        file_name = '../resources/test_inputs/test_user_inputs_2.txt'
+
+        root_folder = utils.get_root_folder()
+        file_path = os.path.join(root_folder, 'resources/test_inputs/test_user_inputs_2.txt')
+
         test_command_class = CommandHandler()
-        test_command_class.set_file_name(file_name)
+        test_command_class.set_file_name(file_path)
         test_command_class.command_runner()
         assert (
                 "3, 3, NORTH"
